a657f98ec7be42594dbbe6c1345e358ec0b52b60
HyunwookPark <park.hyunwook@planaria.co.jp>
2021-01-30 03:08:56 +0900

    REST

--- src/main/java/com/hellospring/controller/HelloRestController.java
+++ src/main/java/com/hellospring/controller/HelloRestController.java
@@ -1,11 +1,26 @@
 package com.hellospring.controller;
 
+import com.hellospring.domain.Todo;
+import com.hellospring.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloRestController {
+
+    @Autowired
+    private TodoService service;
+
+    @RequestMapping("/todos")
+    public List<Todo> list(Model model) {
+        List<Todo> todoList = service.findTodoList();
+        return todoList;
+    }
 
     @RequestMapping("/hello/{name}")
     public String hellorest(@PathVariable String name) {
